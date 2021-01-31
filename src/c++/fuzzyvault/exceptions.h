96b6f9f0ed903bcde2d8881a33a1f03a488e0ed2
KirkOlynyk <kirk.olynyk@gmail.com>
2021-01-31 13:18:43 -0800

    added copyright message

--- src/c++/fuzzyvault/exceptions.h
+++ src/c++/fuzzyvault/exceptions.h
@@ -1,3 +1,13 @@
+/*
+* Copyright 2021 The Decentralized Identity Foundation
+* Project Authors. All Rights Reserved.
+*
+* Licensed under the Apache License 2.0 (the "License"). You may not use
+* this file except in compliance with the License. You can obtain a copy
+* in the file LICENSE in the source distribution or at
+* https://identity.foundation/
+*/
+
 #ifndef _EXCEPTIONS_H_  
 #define _EXCEPTIONS_H_
 
--- src/c++/fuzzyvault/imod.h
+++ src/c++/fuzzyvault/imod.h
@@ -1,3 +1,13 @@
+/*
+* Copyright 2021 The Decentralized Identity Foundation
+* Project Authors. All Rights Reserved.
+*
+* Licensed under the Apache License 2.0 (the "License"). You may not use
+* this file except in compliance with the License. You can obtain a copy
+* in the file LICENSE in the source distribution or at
+* https://identity.foundation/
+*/
+
 #ifndef _IMOD_H_
 #define _IMOD_H_
 
